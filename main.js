4a13fbc51210f3eb2c2a4560ab16b63176b2d4c8
dan <dan@pop-os.localdomain>
2023-02-14 06:42:15 +0900

    custom google map

--- main.js
+++ main.js
@@ -1,23 +1,56 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+let map;
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    mapId: "876806baa0024d81",
+    center: { lat: 40.73090330192132, lng: -73.99024079492982 },
+    zoom: 17,
+  });
 
-setupCounter(document.querySelector('#counter'))
+  const markers = [
+    {
+      title: "一風堂",
+      lat: 40.73090330192132,
+      lng: -73.99024079492982,
+      url: "./javascript.svg",
+      width: 38,
+      height: 38,
+    },
+    {
+      title: "ICHIRAN Ramen ",
+      lat: 40.76010005715904,
+      lng: -73.98328257794572,
+      url: "./javascript.svg",
+      width: 38,
+      height: 38,
+    },
+  ];
+
+  markers.map((currMarker) => {
+
+
+    const marker = new google.maps.Marker({
+      position: { lat: currMarker.lat, lng: currMarker.lng },
+      icon: {
+        url: currMarker.url,
+        scaledSize: new google.maps.Size(currMarker.width, currMarker.height),
+      },
+      map,
+      title: currMarker.title,
+      animation: google.maps.Animation.DROP,
+    });
+
+    const infowindow = new google.maps.InfoWindow({
+      content: currMarker.title,
+    });
+
+    marker.addListener("click", () => {
+      infowindow.open({
+        anchor: marker,
+        map,
+      });
+    });
+  });
+
+}
+window.initMap = initMap;
